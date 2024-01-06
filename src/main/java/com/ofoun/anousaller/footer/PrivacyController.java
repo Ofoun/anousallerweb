d8882d06af955ac9c916028adb33d3717481f385
Jean-Marie TOWANOU <ofoun@yahoo.fr>
2024-01-06 11:25:29 +0100

    Add existing project files to Git

--- src/main/java/com/ofoun/anousaller/footer/PrivacyController.java
+++ src/main/java/com/ofoun/anousaller/footer/PrivacyController.java
@@ -3,6 +3,7 @@ package com.ofoun.anousaller.footer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class PrivacyController {
 	
